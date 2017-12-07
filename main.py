c217bd5847dcfd227ffa1b5b08976658959628d2
Jonas Laksen <jonaslaksen@live.com>
2017-12-07 20:47:23 +0100

    small changes

--- main.py
+++ main.py
@@ -8,20 +8,15 @@ from keras.callbacks import CSVLogger
 import csv
 import cv2
 import numpy as np
+
+from preprocessing import preprocess_image
+
 csv_logger = CSVLogger(time.strftime("./logs/%m-%d-%H:%M:%S"), append=True, separator=';')
 import matplotlib as mpl
 mpl.use('TkAgg')
 import matplotlib.pyplot as plt
+epochs=100
 
-
-def preprocess_image(x, flip=False):
-    x=cv2.imread(x,0)
-    if flip:
-        x = cv2.flip(x,1)
-    x=x[50:150,0:320].copy()
-    x=cv2.resize(x, (160,50))
-    x=x.reshape((50,160,1))
-    return x/255.0
 
 def plot(y):
     y, binEdges = np.histogram(y, bins=100)
@@ -37,10 +32,10 @@ def generate_samples():
     with open('driving_log.csv') as csvfile:
         for center_image,_,_,steering_angle,_,_, speed in csv.reader(csvfile):
             if float(steering_angle)**2 > 0.02 or float(steering_angle)**2 + .02 > random.random():
-                X.append(preprocess_image(center_image))
+                X.append(preprocess_image(cv2.imread(center_image)))
                 y.append(float(steering_angle))
                 if float(steering_angle)**2 > .02:
-                    X.append(preprocess_image(center_image, True))
+                    X.append(preprocess_image(cv2.imread(center_image), True))
                     y.append(-float(steering_angle))
 
     return np.asarray(X),np.asarray(y)
@@ -48,18 +43,32 @@ def generate_samples():
 X, y = generate_samples()
 
 model = Sequential([
-    Conv2D(24, strides=2, kernel_size=5, kernel_regularizer=regularizers.l2(.001), input_shape=(50, 160, 1)),
-    Conv2D(36, strides=2, kernel_size=5, kernel_regularizer=regularizers.l2(.001)),
-    Conv2D(48, strides=1, kernel_size=5, kernel_regularizer=regularizers.l2(.001)),
+    Conv2D(16, strides=3, kernel_size=5, input_shape=(50, 160, 1), kernel_regularizer=regularizers.l2(.001)),
+    # Conv2D(36, strides=2, kernel_size=5, kernel_regularizer=regularizers.l2(.001)),
+    Dropout(.2),
+    Conv2D(32, strides=2, kernel_size=5, kernel_regularizer=regularizers.l2(.001)),
+    Dropout(.2),
+    Conv2D(32, strides=1, kernel_size=5, kernel_regularizer=regularizers.l2(.001)),
+    Dropout(.2),
     Conv2D(64, strides=1, kernel_size=2, kernel_regularizer=regularizers.l2(.001)),
-    Conv2D(64, strides=1, kernel_size=3, kernel_regularizer=regularizers.l2(.001)),
+    Dropout(.2),
     Flatten(),
-    Dense(100, activation='elu', kernel_regularizer=regularizers.l2(.001)),
-    Dense(50, activation='elu', kernel_regularizer=regularizers.l2(.001)),
-    Dense(10, activation='elu', kernel_regularizer=regularizers.l2(.001)),
+    Dense(1024, activation='elu', kernel_regularizer=regularizers.l2(.001)),
+    Dropout(.5),
+    Dense(256, activation='elu', kernel_regularizer=regularizers.l2(.001)),
+    Dropout(.5),
+    Dense(128, activation='elu', kernel_regularizer=regularizers.l2(.001)),
+    Dropout(.5),
     Dense(1)
 ])
 
 model.compile(loss='mean_squared_error', optimizer=Adam())
-history = model.fit(X,y,batch_size=32,epochs=50,callbacks=[csv_logger], validation_split=0.1, verbose=2)
+history = model.fit(X,y,batch_size=32,epochs=epochs,callbacks=[csv_logger], validation_split=0.1, verbose=2)
+
+plt.plot(range(epochs),history.history['loss'], label='Training loss' )
+plt.plot( range(epochs), history.history['val_loss'], label='Validation loss')
+plt.axis([0,epochs,0,max([max(history.history['loss']), max(history.history['val_loss'])])])
+plt.xlabel('Epoch')
+plt.legend()
+plt.show()
 model.save('model.h5')
