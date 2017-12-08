0ed11c7d50d8682f1b88099d4b45aaea2bf4c332
Jonas Laksen <jonaslaksen@live.com>
2017-12-08 18:06:11 +0100

    lol

--- main.py
+++ main.py
@@ -58,8 +58,8 @@ model = Sequential([
     Flatten( ),
     Dense(32, activation='elu', kernel_regularizer=regularizers.l2(.001)),
     Dropout(.5),
-    Dense(16, activation='elu', kernel_regularizer=regularizers.l2(.001)),
-    Dropout(.5),
+    # Dense(16, activation='elu', kernel_regularizer=regularizers.l2(.001)),
+    # Dropout(.5),
     Dense(1)
 ])
 
