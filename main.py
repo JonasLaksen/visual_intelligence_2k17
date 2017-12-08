3e8a75a5dbf0ff8a641591737576abe04fd7a0af
Jonas Laksen <jonaslaksen@live.com>
2017-12-08 19:54:58 +0100

    lol

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def generate_samples():
         for center_image,_,_,steering_angle,_,_, speed in csv.reader(csvfile):
             # if counter >2000: break
             # counter +=1
-            if float(steering_angle)**2 > 0.02 or float(steering_angle)**2 + .08 > random.random():
+            if float(steering_angle)**2 > 0.02 or random.random() < .2:
                 X.append(preprocess_image(cv2.imread(center_image)))
                 y.append(float(steering_angle))
             if float(steering_angle)**2 > .02:
