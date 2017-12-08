91b9a0cd5e42ede5cdde84014e2669907206fce1
Jonas Laksen <jonaslaksen@live.com>
2017-12-08 15:04:27 +0100

    lol

--- main.py
+++ main.py
@@ -30,10 +30,10 @@ def plot(y):
 def generate_samples():
     X,y = [],[]
     with open('driving_log.csv') as csvfile:
-        counter = 0
+        # counter = 0
         for center_image,_,_,steering_angle,_,_, speed in csv.reader(csvfile):
-            if counter >6800: break
-            counter +=1
+            # if counter >6800: break
+            # counter +=1
             if float(steering_angle)**2 > 0.02 or float(steering_angle)**2 + .02 > random.random():
                 X.append(preprocess_image(cv2.imread(center_image)))
                 y.append(float(steering_angle))
@@ -57,7 +57,7 @@ model = Sequential([
     # Dropout(.5),
     Flatten( input_shape=(160, 320, 3)),
     # Dense(32, activation='elu'),
-    Dense(16, activation='elu'),
+    # Dense(16, activation='elu'),
     Dense(8, activation='elu'),
     Dense(1)
 ])
