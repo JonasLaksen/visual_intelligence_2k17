34ad2f1a6b51b1440c6364c1c46b4e4cf744fd9d
Jonas Laksen <jonaslaksen@live.com>
2017-12-08 15:49:53 +0100

    lol

--- main.py
+++ main.py
@@ -48,8 +48,8 @@ X, y = generate_samples()
 model = Sequential([
     Conv2D(8, strides=3, kernel_size=3, input_shape=(160, 320, 3), kernel_regularizer=regularizers.l2(.001), activation='elu'),
     Dropout(.2),
-    # Conv2D(16, strides=3, kernel_size=3, kernel_regularizer=regularizers.l2(.001), activation='elu'),
-    # Dropout(.2),
+    Conv2D(16, strides=3, kernel_size=3, kernel_regularizer=regularizers.l2(.001), activation='elu'),
+    Dropout(.2),
     # Conv2D(32, strides=3, kernel_size=3, kernel_regularizer=regularizers.l2(.001), activation='elu'),
     # Dropout(.2),
     Flatten(),
